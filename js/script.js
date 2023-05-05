9941ff01aa924c190ecfee06e9b948643256637a
jaciteixeira <rm99627@fiap.com.br>
2023-05-05 10:54:39 -0300

    Todo conteudo da aula anterior comentado

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
-/* <button id="meu-btn">CLICK</button> */
+// /* <button id="meu-btn">CLICK</button> */
 
-const btn = document.getElementById("meu-btn");
+// const btn = document.getElementById("meu-btn");
 
-// console.log(btn.textContent);
-//Atrelando um evento ao elemento
-btn.addEventListener("click",function(){
+// // console.log(btn.textContent);
+// //Atrelando um evento ao elemento
+// btn.addEventListener("click",function(){
 
-    //Função matemática Math
-    //Math.random = retorna numeros aleatorios entre 0 e 1. Exemplo: 0,12182
-    //Math.floor = Função de arrendodamento para baixo 
-    //Math.ceil = Função de arrendodamento para cima 
-    //Math.round = Função de arrendodamento aleatorio
+//     //Função matemática Math
+//     //Math.random = retorna numeros aleatorios entre 0 e 1. Exemplo: 0,12182
+//     //Math.floor = Função de arrendodamento para baixo 
+//     //Math.ceil = Função de arrendodamento para cima 
+//     //Math.round = Função de arrendodamento aleatorio
     
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    //numeros aleatorios gerados...
-    r = Math.round(Math.random()*255);
-    g = Math.round(Math.random()*255);
-    b = Math.round(Math.random()*255);
+//     //numeros aleatorios gerados...
+//     r = Math.round(Math.random()*255);
+//     g = Math.round(Math.random()*255);
+//     b = Math.round(Math.random()*255);
 
-    // this.setAttribute("style","background-color:rgb("+r+","+g+","+b+");")
-    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+//     // this.setAttribute("style","background-color:rgb("+r+","+g+","+b+");")
+//     this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
 
-    console.log(this.textContent);
-});
+//     console.log(this.textContent);
+// });
 
 
-const tit_sec = document.getElementById("tit-sec");
+// const tit_sec = document.getElementById("tit-sec");
 
-tit_sec.addEventListener("click", function(){
-    let num = 0;
-    num = Math.round(Math.random()*10);
+// tit_sec.addEventListener("click", function(){
+//     let num = 0;
+//     num = Math.round(Math.random()*10);
 
-    this.setAttribute("style", `font-size: ${num}px;`);
-})
-
-/*NÃO DEU CERTO !*/
-// cor = document.querySelector(".h2-rodape");
-// cor.addEventListener("click", function(){
-//     this.addEventListener("style", "background-color:red");
+//     this.setAttribute("style", `font-size: ${num}px;`);
 // })
+
+// /*NÃO DEU CERTO !*/
+// // cor = document.querySelector(".h2-rodape");
+// // cor.addEventListener("click", function(){
+// //     this.addEventListener("style", "background-color:red");
+// // })
