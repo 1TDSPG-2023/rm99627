59e59985ab7d66d587ba7ce2319013ecaea4c921
jaciteixeira <rm99627@fiap.com.br>
2023-05-05 12:50:43 -0300

    Arrays e seus métodos/forEach

--- js/script.js
+++ js/script.js
@@ -42,3 +42,51 @@
 // // cor.addEventListener("click", function(){
 // //     this.addEventListener("style", "background-color:red");
 // // })
+
+
+
+
+
+
+//ARRAYS E SEU M[ETODOS]
+let frutas = ["banana","maçã","pêra","uva","melancia","ameixa","morango","goiaba"];
+console.log(frutas);
+console.log("FRUTA : " + frutas[4])
+//Descobrindo o tamanho do array através da propriedade length
+console.log(`Tamanho do Array : ${frutas.length}`);
+
+//Adicionando um iten ao final do array com o método push(itenNome);
+frutas.push("fruta-conde");
+console.table(frutas);
+//Adicionando um iten no inicio do array com o método unshift(itenNome);
+frutas.unshift("jabuticaba");
+console.log(frutas);
+
+//Remover um iten do final do array com o método pop();
+frutas.pop();
+console.log(frutas);
+//Remover um iten do inicio do array com o método shift();
+frutas.shift();
+console.log(frutas);
+
+//Localizando um item com o método indexOf(itenNome);
+//Obs: O indexOf(itenNome), vai retornar o indice onde se encontra o iten buscado.
+// let indice = frutas.indexOf("morango");
+// console.log(indice);
+// console.log(frutas[indice]);
+
+//Removendo um indice uma determinada quantidade de vezes utilizando o método splice.
+// splice(indice do iten, quantidade de vezes a ser removido);
+//Obs: Para obter o indice utilize o método indexOf(itenNome);
+let indice = frutas.indexOf("ameixa");
+console.log(indice);
+console.log(`FRUTA QUE ESTAVA NO INDICE ANTES DA REMOÇÃO : ${frutas[indice]}`);
+//Aplicando o splice e usando o indice obtido com indexOf.
+frutas.splice(indice,1);
+console.log(`FRUTA QUE FICOU NO INDICE DEPOIS DA REMOÇÃO : ${frutas[indice]}`);
+
+//Percorrer o ARRAY utilizando o forEach
+frutas.forEach( (fruta)=>{
+    console.log(`Olha ele: ${fruta}`);
+} );
+
