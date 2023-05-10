040b784cd02bf2aca18e28e4fb777f0f9f43fc31
jaciteixeira <rm99627@fiap.com.br>
2023-05-10 09:36:12 -0300

    usando for.each

--- js/login.js
+++ js/login.js
@@ -9,16 +9,30 @@ const usuario1 = {
     gravaDados: true,
     token: tokenGerado
 }
+const usuario2 = {
+    nomeUsuario: "gege",
+    senhaUsuario: "12345",
+    gravaDados: true,
+    token: tokenGerado
+}
+
+let listaDeUsuarios = []
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
 
 addEventListener("click", (evt) => {
     if(evt.target.id == "btnSubmit") {
         const inputUser = document.querySelector("#idUser");
         const inputPass = document.querySelector("#idPass");
 
-        if (inputUser.value == "pf0670" && inputPass.value == "12345") {
-            console.log("VALIDADO");
-        } else {
-            console.log("NÃO VALIDADO");
-        }
+        
+        listaDeUsuarios.forEach((usuario) => {
+            if (inputUser.value == usuario.nomeUsuario && inputPass.value == usuario.senhaUsuario) {
+                console.log("VALIDADO");
+            } else {
+                console.log("NÃO VALIDADO");
+            }
+        });
     }
 })
